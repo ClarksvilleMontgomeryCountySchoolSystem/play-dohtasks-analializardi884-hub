2a79bdd7ffe1ad263b2cbb230cf42d6235038bd9
analializardi884-hub <analia.lizardi884@students.cmcss.net>
2026-01-23 09:47:27 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
-# Enter your color.
+def main():
+    # Enter your color.
     color1 = "green"
 
     print(f"1) Use {color1} to roll a medium-sized ball.\n")
@@ -24,3 +25,6 @@
        print("7) Keep the small ball round.\n")
     print("8) Poke two tiny holes in the small ball for eyes.\n")
     print('9) Write "Turtle" on the name card.')
+
+if __name__ == "__main__":
+    main()
